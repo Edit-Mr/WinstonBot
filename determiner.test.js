049ea6e4058c8f35d5a1a3c34195774a2c6cecbe
Yi-Jyun Pan <me@pan93.com>
2025-03-05 21:38:23 +0800

    test: Add testcases
    Signed-off-by: Yi-Jyun Pan <me@pan93.com>

---
+++ determiner.test.js
@@ -0,0 +1,88 @@
+//@ts-check
+
+import { suite } from "vitest";
+import { Determiner } from "./determiner";
+import { expect } from "vitest";
+import { test } from "vitest";
+
+suite("Determiner", () => {
+    test("繁體中文規則應該套用在繁體中文上", () => {
+        /**
+         * @type {import("./determiner").SpellingRule[]}
+         */
+        const rules = [
+            {
+                wrong: "你好",
+                correct: "Hello",
+                caseSensitive: false,
+                traditionalOnly: true,
+            },
+        ];
+
+        const determiner = new Determiner(rules);
+        const text = "繁體中文你好";
+
+        const result = determiner.checkSpelling(text);
+        expect(result).toHaveLength(1);
+    });
+
+    test("繁體中文規則應該套用在中性規則上", () => {
+        /**
+         * @type {import("./determiner").SpellingRule[]}
+         */
+        const rules = [
+            {
+                wrong: "你好",
+                correct: "Hello",
+                caseSensitive: false,
+                traditionalOnly: true,
+            },
+        ];
+
+        const determiner = new Determiner(rules);
+        const text = "你好";
+
+        const result = determiner.checkSpelling(text);
+        expect(result).toHaveLength(1);
+    });
+
+    test("繁體中文規則不該套用在簡體中文上", () => {
+        /**
+         * @type {import("./determiner").SpellingRule[]}
+         */
+        const rules = [
+            {
+                wrong: "你好",
+                correct: "Hello",
+                caseSensitive: false,
+                traditionalOnly: true,
+            },
+        ];
+
+        const determiner = new Determiner(rules);
+        const text = "简体中文你好";
+
+        const result = determiner.checkSpelling(text);
+        expect(result).toHaveLength(0);
+    });
+
+    test("非繁體中文規則應該套用在簡體中文上", () => {
+        /**
+         * @type {import("./determiner").SpellingRule[]}
+         */
+        const rules = [
+            {
+                wrong: "你好",
+                correct: "Hello",
+                caseSensitive: false,
+                traditionalOnly: false,
+            },
+        ];
+
+        const determiner = new Determiner(rules);
+        const text = "简体中文你好";
+
+        const result = determiner.checkSpelling(text);
+        expect(result).toHaveLength(1);
+    })
+})
