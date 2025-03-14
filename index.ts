65c889ae7102b9005475da5d384a8e17484311ad
Yi-Jyun Pan <me@pan93.com>
2025-03-14 19:01:33 +0800

    feat: Adapt new message

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
 import { Determiner } from "./determiner.ts";
-import { SpellingCheckDatabase } from "./database.ts";
+import { SpellingDatabase, CaseDatabase } from "./database.ts";
 
 // 載入環境變數
 config();
@@ -20,7 +20,9 @@ if (!mongodbUri) {
     throw new Error("MONGODB_URI is not defined");
 }
 
-const database = await SpellingCheckDatabase.fromConnectionString(mongodbUri);
+const spellingDatabase = await SpellingDatabase.fromConnectionString(mongodbUri);
+const caseDatabase = await CaseDatabase.fromConnectionString(mongodbUri);
+const determiner = new Determiner(spellingDatabase, caseDatabase);
 
 // 當 bot 準備就緒時
 client.once(Events.ClientReady, async () => {
@@ -34,16 +36,27 @@ client.on(Events.MessageCreate, async message => {
     // 忽略 bot 的訊息
     if (message.author.bot) return;
     try {
-        const determiner = new Determiner(database);
-
         const mistakes = await determiner.checkSpelling(message.content);
 
         if (mistakes.length > 0) {
             const response = mistakes
-                .map(
-                    mistake =>
-                        `~~《${mistake.wrong}》（✗）~~\n「${mistake.correct}」（✓）\n`
-                )
+                .map(mistake => {
+                    const correctTerms = mistake.correct.join("、");
+                    switch (mistake.type) {
+                        case "case":
+                            return `「${mistake.correct}」是專有名詞，請注意拼法及大小寫（你拼成 ${mistake.wrong}）。\n`;
+                        case "political_terminology":
+                            return `「${mistake.wrong}」為含有政治色彩的中國用語，請改用「${correctTerms}」\n`;
+                        case "regional_difference":
+                            return `「${mistake.wrong}」為兩岸用法不同的中國用語，台灣用語是「${correctTerms}」\n`;
+                        case "spelling_correction":
+                            return `錯字，「${mistake.wrong}」應寫成「${correctTerms}」\n`;
+                        case "term_ambiguity_check":
+                            return `你確定你想說的是「${mistake.wrong}」而不是「${correctTerms}」嗎？\n`;
+                        default:
+                            return `「${mistake.wrong}」應改為「${correctTerms}」\n`;
+                    }
+                })
                 .join("");
 
             await message.reply(response);
